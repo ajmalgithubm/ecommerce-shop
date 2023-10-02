a1d6cdbeb44c58f1316006571bcde58fbaa7890d
ajmalgithubm <ajmalm76774@gmail.com>
2023-10-02 22:45:28 +0530

    every route the auth middleware added

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,3 +1,15 @@
 import JWT from 'jsonwebtoken';
 
-// pro
+// protected Routes token base
+export const requireSignIn = async (req, res, next) => {
+    try{
+        const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET_KEY);
+        next();
+    }catch(err){
+        console.log(err)
+
+    }
+}
+
+// admin access
+export
--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { loginController, registerController } from "./../Controllers/authController.js";
+import { loginController, registerController, testController } from "./../Controllers/authController.js";
+import { requireSignIn } from "../Middleware/authMiddleware.js";
 
 // router object
 const router = express.Router();
@@ -11,6 +12,9 @@ const router = express.Router();
 router.post("/register", registerController);
 
 // login router
-router.post("/login", loginController)
+router.post("/login", loginController);
+
+// test  routes
+router.get("/test", requireSignIn ,testController);
 
 export default router;
